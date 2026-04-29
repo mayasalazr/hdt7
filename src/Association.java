480020c6e2ec7e8dcb7d1587f38b31a725b7a2ff
mayasalazr <salazrmaya@gmail.com>
2026-04-28 19:25:35 -0600

    Modificacion en asociacion

--- src/Association.java
+++ src/Association.java
@@ -6,7 +6,22 @@ public class Association<K extends Comparable<K>, V>{
         this.key = key;
         this.value = value
     }
+
+
+    @Override
+    public int compareTo(Association<K, V> other){
+        return this.key.compareTo(other.key);
+
+
+    }
+
+    @Override
+    public String toString(){
+        return "(" + key + ", " + value + ")";
+    }
 }
+
+
 
 
 
